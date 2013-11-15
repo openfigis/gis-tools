d823cbdce6e86f51869eed29a35577e6b73dc884
eblondel <emmanuel.blondel1@gmail.com>
2013-11-15 15:20:02 +0100

    fix typo

--- gis-metadata/gis-metadata/collection/src/main/java/org/fao/fi/gis/metadata/MetadataGenerator.java
+++ gis-metadata/gis-metadata/collection/src/main/java/org/fao/fi/gis/metadata/MetadataGenerator.java
@@ -50,7 +50,7 @@ public class MetadataGenerator {
 
 		//Read the configuration
 		LOGGER.info("(1) Loading the configuration file");
-		MetadataConfig config = MetadataConfig.fromXML(new File("c:/gis/metadata/config/species.xml"));
+		MetadataConfig config = MetadataConfig.fromXML(new File(args[0]));
 		
 		//read the codelists
 		LOGGER.info("(2) Loading the reference list");

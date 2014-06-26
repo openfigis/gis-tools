24858998458e547e183f4e736cd7c31c5d15946b
eblondel <emmanuel.blondel1@gmail.com>
2014-06-26 09:29:17 +0200

    -

--- gis-metadata/gis-metadata/main/src/main/java/org/fao/fi/gis/metadata/GeographicEntityMetadata.java
+++ gis-metadata/gis-metadata/main/src/main/java/org/fao/fi/gis/metadata/GeographicEntityMetadata.java
@@ -481,7 +481,6 @@ public class GeographicEntityMetadata extends DefaultMetadata {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
 	private void setIdentificationInfo() throws URISyntaxException,
 			ParseException {
 
